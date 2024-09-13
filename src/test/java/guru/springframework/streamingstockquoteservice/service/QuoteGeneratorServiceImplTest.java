440facbf8e9ee66424fdf7ed74d8c0c9c29c9079
Maria-del-Mar <mmvillarte@hotmail.com>
2024-09-13 09:35:40 +0200

    Testing with lambdas

---
+++ src/test/java/guru/springframework/streamingstockquoteservice/service/QuoteGeneratorServiceImplTest.java
@@ -0,0 +1,41 @@
+package guru.springframework.streamingstockquoteservice.service;
+
+import guru.springframework.streamingstockquoteservice.model.Quote;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class QuoteGeneratorServiceImplTest {
+
+    QuoteGeneratorService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new QuoteGeneratorServiceImpl();
+    }
+
+    @Test
+    void fetchQuoteStream() throws InterruptedException {
+        Flux<Quote> quoteFlux = service.fetchQuoteStream(Duration.ofMillis(100l));
+
+        Consumer<Quote> quoteConsumer = System.out::println;
+
+        Consumer<Throwable> throwableConsumer = e -> log.info("Error message: ", e.getMessage());
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        Runnable done = () -> countDownLatch.countDown();
+
+        quoteFlux.take(30).subscribe(quoteConsumer, throwableConsumer, done);
+
+        countDownLatch.await();
+    }
+}
